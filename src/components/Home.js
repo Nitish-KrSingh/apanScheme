97b61d3bc7d3ebfa3ce194e25ba68630670ce7fe
Tanushree227 <tanushreedas813@gmail.com>
2023-03-19 17:28:21 +0530

    changes made

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,11 @@ import './Home.css';
 
 const Home = () => {
     return (
-      <div className="container d-flex align-items-center h-100">
+      <div className="container d-flex align-items-center h-1000">
     <div className="row">
       <header className="text-center col-12">
         <h1 className="text-uppercase"><strong>#GOVERNMENTSCHEMESFORYOU</strong></h1>
+        <h3>Discover all Government schemes on one stop portal.</h3>
       </header>
       <div className="buffer col-12"></div>
       <section className="text-center col-12">
