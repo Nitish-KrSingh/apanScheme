9d9b69984bf44d3f158503f6ae18345066bd0b1c
Tanushree227 <tanushreedas813@gmail.com>
2023-04-12 10:13:43 +0530

    commit changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,7 @@ import States from './States/States';
 import Ministries from "./Ministries/Ministries"
 import { useNavigate } from "react-router-dom"
 import Footer from "../Footer/Footer"
+import Page from "./Page/Page";
 
 
 const Home = () => {
@@ -56,6 +57,7 @@ const Home = () => {
         {schemeFilter === 'states' && <States/>}
         {schemeFilter === 'ministries' && <Ministries/>}
       </div>
+      <Page />
       <Footer/>
   </div>
   
