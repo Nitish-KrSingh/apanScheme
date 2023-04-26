561eacaef7583fdd3ab23d7c7a961b56b764d8cd
Nitish <nitishbcps@gmail.com>
2023-04-26 10:08:09 +0530

    ui changes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,11 @@
 import Card from './ui/Card';
-import classes from './Signup.module.css';
+import classNamees from './Signup.module.css';
 import { AuthContext } from '../context/auth-context';
 import { useContext, useEffect } from 'react';
 import { postSignup } from '../api/auth-api';
 import { Navigate, redirect, useNavigate } from 'react-router-dom';
 import { UiContext } from '../context/ui-context';
+import SignupImage from '../assets/images/SignUpImg.jpg';
 
 const Signup = () => {
     const authContext = useContext(AuthContext);
@@ -55,34 +56,48 @@ const Signup = () => {
     }
 
     return (
-        <Card>
-        <h2>Sign-Up</h2>
-      <form onSubmit={signupSubmitHandler} className={classes.form}>
-        <div className={classes.control}>
-          <label htmlFor='name'>Name</label>
-          <input type='text' name='name' required id='name' />
+      <>
+      <section className="h-auto">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 text-black">
+              <Card>
+              <h2>Sign-Up</h2>
+            <form onSubmit={signupSubmitHandler} className={classNamees.form}>
+              <div className={classNamees.control}>
+                <label htmlFor='name'>Name</label>
+                <input type='text' name='name' required id='name' />
+              </div>
+              <div className={classNamees.control}>
+                <label htmlFor='phone'>Phone Number</label>
+                <input type='number' name='phone' required id='phone' />
+              </div>
+              <div className={classNamees.control}>
+                <label htmlFor='email'>Email</label>
+                <input type='email' required id='email' />
+              </div>
+              <div className={classNamees.control}>
+                <label htmlFor='password'>Password</label>
+                <input id='password' required rows='5' />
+              </div>
+              <div className={classNamees.control}>
+                <label htmlFor='cpassword'>Confirm Password</label>
+                <input id='cpassword' required rows='5' />
+              </div>
+              <div className={classNamees.actions}>
+                <button>Sign-Up</button>
+              </div>
+            </form>
+           
+          </Card>
+          </div>
+          <div className="col-sm-6 px-0 d-none d-sm-block">
+          <img src={SignupImage} alt="Login image" className="w-100 vh-80 signupimg" />
         </div>
-        <div className={classes.control}>
-          <label htmlFor='phone'>Phone Number</label>
-          <input type='number' name='phone' required id='phone' />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='email'>Email</label>
-          <input type='email' required id='email' />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Password</label>
-          <input id='password' required rows='5' />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='cpassword'>Confirm Password</label>
-          <input id='cpassword' required rows='5' />
-        </div>
-        <div className={classes.actions}>
-          <button>Sign-Up</button>
-        </div>
-      </form>
-    </Card>
+      </div>
+    </div>
+  </section>
+    </>
     )
 }
 
