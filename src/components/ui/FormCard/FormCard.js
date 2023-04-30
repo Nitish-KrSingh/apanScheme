3172344dbc4ad67553273578b658bc3b420846e3
Nitish <nitishbcps@gmail.com>
2023-05-01 03:21:24 +0530

    form added

--- src/components/ui/FormCard/FormCard.js
+++ src/components/ui/FormCard/FormCard.js
@@ -1,11 +1,11 @@
 function FormCard(props) {
     return (
-        <section className="vh-100">
+        <section className="mb-5">
             <div className="mask d-flex align-items-center mt-5">
-                <div className="container h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="container mh-100">
+                    <div className="row d-flex justify-content-center align-items-center mh-100">
                         <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-                            <div className="card " style={{"border-radius" :"15px"}}>
+                            <div className="card " style={{"border-radius" :"15px" , height: "450px" }}>
                                 <div className="card-body p-5">
                                     {props.children}
 
